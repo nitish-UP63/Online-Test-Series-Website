f9d85136ab80661ae5da8a3a374494b6ac4e9f34
RAVINDRA PRATAP PATEL <ravindra955440@gmail.com>
2021-12-30 22:38:31 +0530

    created footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from './component/Footer'
 
 export default function App() {
   return (
     <div>
       <h1>wqdoidqiodoqi</h1>
+      <Footer />
     </div>
   )
 }
---
+++ src/component/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Footer.css';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+function Footer() {
+    return (
+        <div className='footer'> 
+        <div className='main'>
+            <div className='col1'>
+             <h1>Reach to Us</h1>
+             <div> 
+             <p>Email</p>
+             <p>Contact</p>
+            
+             </div>
+            </div>
+            <div className='col2'>
+            <h1>Category</h1>
+            <div>
+                <p>JEE Main</p>
+                <p>NEET</p>
+                <p>AKTU</p>
+                <p>NDA</p>
+            </div>
+            </div>
+            <div className='col3' >
+            <h1>Follow Us</h1>
+            <div>
+             <p>  <InstagramIcon  fontSize='5' />Instagram</p>
+             <p>  <FacebookIcon fontSize='5' />   Facebook</p>
+             <p>  <YouTubeIcon fontSize='5' />   Youtube</p>
+             <p> <TwitterIcon fontSize='5' />   Twitter</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Footer;
