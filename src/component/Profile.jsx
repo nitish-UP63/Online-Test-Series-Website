e721320552e14fc3c3d27567d43096936b9a8628
Nitish kumar <nitishiiitian@gmail.com>
2022-01-02 17:07:26 +0530

    profile

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -5,14 +5,14 @@ import "./profile.css"
 export default function Profile() {
     return (
         <div>
-            <div classNameName="container rounded bg-white mt-5 mb-5">
-                <div classNameName="row">
-                    <div classNameName="col-md-3 border-right">
-                        <div classNameName="d-flex flex-column align-items-center text-center p-3 py-5"><img classNameName="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" /><span classNameName="font-weight-bold">Edogaru</span><span classNameName="text-black-50">edogaru@mail.com.my</span><span> </span></div>
+            <div className="container rounded bg-white mt-5 mb-5">
+                <div className="row">
+                    <div className="col-md-3 border-right">
+                        <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" /><span className="font-weight-bold">Edogaru</span><span className="text-black-50">edogaru@mail.com.my</span><span> </span></div>
                     </div>
-                    <div classNameName="col-md-5 border-right">
-                        <div classNameName="p-3 py-5">
-                            <div classNameName="d-flex justify-content-between align-items-center mb-3">
+                    <div className="col-md-5 border-right">
+                        <div className="p-3 py-5">
+                            <div className="d-flex justify-content-between align-items-center mb-3">
                                 <h4 className="text-right">Profile Settings</h4>
                             </div>
                             <div className="row mt-2">
