2efe047a9eff4e4526bdc2b4f07e3a0ee5bb63f9
Nitish kumar <nitishiiitian@gmail.com>
2022-01-04 18:31:00 +0530

    Profile update

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -17,27 +17,21 @@ export default function Profile() {
                     <div className="col-md-5 border-right">
                         <div className="p-3 py-5">
                             <div className="d-flex justify-content-between align-items-center mb-3">
-                                <h4 className="text-right">Profile Settings</h4>
+                                <h4 className="text-right">Profile </h4>
                             </div>
                             <div className="row mt-2">
-                                <div className="col-md-6"><label className="labels">Name</label><input type="text" className="form-control" placeholder="first name"  /></div>
-                                <div className="col-md-6"><label className="labels">Surname</label><input type="text" className="form-control" placeholder="surname" /></div>
+                                <div className="col-md-8"><label className="labels">Name</label><input type="text" className="form-control" placeholder="first name"  /></div>
+                                
                             </div>
                             <div className="row mt-3">
-                                <div className="col-md-12"><label className="labels">Mobile Number</label><input type="text" className="form-control" placeholder="enter phone number"  /></div>
-                                <div className="col-md-12"><label className="labels">Address</label><input type="text" className="form-control" placeholder="address"  /></div>
+                                <div className="col-md-6"><label className="labels">Mobile Number</label><input type="text" className="form-control" placeholder="enter phone number"  /></div>
+                               
                                 
                                 
-                                <div className="col-md-12"><label className="labels">Email ID</label><input type="text" className="form-control" placeholder="enter email id"  /></div>
-                                <div className="col-md-12"><label className="labels">Education</label><input type="text" className="form-control" placeholder="education"  /></div>
-                                <div className="col-md-12"><label className="labels">Target</label><input type="text" className="form-control" placeholder="Prepairing for which paper"  /></div>
-                            </div>
-                            <div className="row mt-3">
-                            <div className="col-md-6"><label className="labels">Pin Code</label><input type="text" className="form-control" placeholder="pin Code"  /></div>
+                                <div className="col-md-6"><label className="labels">Email ID</label><input type="text" className="form-control" placeholder="enter email id"  /></div>
+                               
+                                <div className="col-md-6"><label className="labels">Target</label><input type="text" className="form-control" placeholder="Prepairing for which exam"  /></div>
                                 <div className="col-md-6"><label className="labels">City</label><input type="text" className="form-control" placeholder="city"  /></div>
-                                <div className="col-md-6"><label className="labels">State</label><input type="text" className="form-control" placeholder="state"  /></div>
-                                <div className="col-md-6"><label className="labels">Country</label><input type="text" className="form-control" placeholder="country"  /></div>
-                
                             </div>
                             <div className="mt-5 text-center"><button className="btn btn-primary profile-button" type="button">Save Profile</button></div>
                         </div>
--- src/component/QuestionPage.jsx
+++ src/component/QuestionPage.jsx
@@ -54,7 +54,7 @@ export default function QuestionPage() {
 
 
     return (
-        <div>
+        <div style={{overflow:"hidden"}}>
             <div className='row d-flex justify-content-between align-items-center'>
              {inst ? <><div className='col-md-8 border-right d-flex flex-column' >
           { 
